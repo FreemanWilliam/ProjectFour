2e0be3061735c405ce11d9187946be724eec90b0
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-07-09 16:13:56 -0400

    Added a New Column
    Added a new column to the produceItem table called description.

--- vagrant/Produce/database_setup.py
+++ vagrant/Produce/database_setup.py
@@ -24,6 +24,7 @@ class ProduceItem(Base):
 
 	name = Column(String(80), nullable=False)
 	id = Column(Integer, primary_key=True)
+	description = Column()
 
 engine = create_engine(
 'sqlite:///producemenu.db')
