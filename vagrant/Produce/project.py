fec95645c22b12ee8658a66643fddead6bc7221e
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-07-26 17:04:18 -0400

    Changed Test Message
    Changed the test message returned by the Produce function.

--- vagrant/Produce/project.py
+++ vagrant/Produce/project.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/produce')
 def Produce():
-	return "This is the Produce Function."
+	return "This is the new Produce Function."
 	
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0', port = 5000)
